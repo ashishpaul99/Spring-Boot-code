c25ff3c27cf9054bfd2652732b4aa9207fc02296
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-01-28 20:23:03 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+package com.ashish.first;
+
+import org.springframework.stereotype.Component;
+
+@Component //spring says I am responsible to create object of Laptop class
+public class Laptop {
+
+	public void compile() {
+		// TODO Auto-generated method stub
+		System.out.println("compile");
+		
+	}
+
+}
